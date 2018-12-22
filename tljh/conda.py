6191c241ff5d4ba3717f92f6932824d362213186
Steve Parker <steven@stevenparker.com>
2018-12-22 06:15:56 -0500

    Fixing Install Point for MiniConda.

--- tljh/conda.py
+++ tljh/conda.py
@@ -5,6 +5,7 @@ import os
 import subprocess
 import json
 import hashlib
+import logger
 import contextlib
 import tempfile
 import urllib.request
@@ -50,7 +51,6 @@ def download_miniconda_installer(version, md5sum):
     """
     with tempfile.NamedTemporaryFile() as f:
         installer_url = "https://repo.continuum.io/miniconda/Miniconda3-{}-Linux-x86_64.sh".format(version)
-        logger.info("Installing from: %s" % installer_url)
         urllib.request.urlretrieve(installer_url, f.name)
 
         if md5_file(f.name) != md5sum:
